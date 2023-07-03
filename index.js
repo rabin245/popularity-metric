c4d04b6f83f3af9a7d36e84eadd3ed2d95826f6a
rabin245 <drarabin@gmail.com>
2023-07-03 22:18:08 +0545

    track time only on required pages

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ import debounce from "./debounce";
 import throttle from "./throttle";
 import * as store from "./store";
 import * as weightedAverage from "./weightedAverage";
-import { checkPathNameAndStoreTime } from "./pathUtils";
+import {
+  checkPathNameAndStoreTime,
+  setupHistoryListener,
+  isUserActive,
+  updateEndTime,
+} from "./utils";
 
 // constants for tracking time on page
 const INTERVAL_TIME = 10000;
@@ -96,20 +101,22 @@ function trackTimeOnPages({ weight, patterns }) {
     startInterval();
   }
 
-  startInterval();
+  // instead of starting the interval with the function, use the custom event to start the interval
+  // startInterval();
 
   // restart interval when document is visible
-  document.addEventListener("visibilitychange", () => {
+  function handleVisibilityChange() {
     console.log("visibility changed", document.hidden);
     if (document.hidden) {
       endTime = Date.now();
     } else {
       restartInterval();
       startTime = Date.now();
-      endTime = updateEndTime(startTime);
+      endTime = updateEndTime(startTime, INTERVAL_TIME);
     }
     console.log(startTime, endTime, endTime - startTime);
-  });
+  }
+  // document.addEventListener("visibilitychange", handleVisibilityChange);
 
   function addTime() {
     console.log(
@@ -122,54 +129,55 @@ function trackTimeOnPages({ weight, patterns }) {
       console.log("user is not active");
       restartInterval();
     }
-    endTime = updateEndTime(startTime);
+    endTime = updateEndTime(startTime, INTERVAL_TIME);
     console.log(startTime, endTime, endTime - startTime);
   }
 
   // add time to endTime when user is active on the page
   // i.e. when user is scrolling, moving mouse, typing, etc.
   const throttleAddTime = throttle(addTime, CHECK_TIME);
-  events.forEach((event) => {
-    document.addEventListener(event, throttleAddTime);
-  });
+  // events.forEach((event) => {
+  //   document.addEventListener(event, throttleAddTime);
+  // });
 
   // dispatch custom event when location changes
   setupHistoryListener();
 
   // add time to endTime when location changes
-  window.addEventListener("locationchange", () => {
+  window.addEventListener("locationchange", handleLocationChange);
+  function handleLocationChange() {
     console.log("location chagned");
-    restartInterval();
-    endTime = updateEndTime(startTime);
-  });
-}
 
-function isUserActive(startTime, endTime) {
-  return !document.hidden && startTime <= endTime;
-}
+    if (intervalId) stopInterval();
 
-function updateEndTime(startTime) {
-  return startTime + INTERVAL_TIME;
-}
+    const currentPath = window.location.pathname;
 
-function setupHistoryListener() {
-  let oldPushState = history.pushState;
-  history.pushState = function pushState() {
-    let ret = oldPushState.apply(this, arguments);
-    window.dispatchEvent(new Event("locationchange"));
-    return ret;
-  };
+    events.forEach((event) => {
+      console.log("removing event listeners");
+      document.removeEventListener(event, throttleAddTime);
+    });
+    document.removeEventListener("visibilitychange", handleVisibilityChange);
 
-  let oldReplaceState = history.replaceState;
-  history.replaceState = function replaceState() {
-    let ret = oldReplaceState.apply(this, arguments);
-    window.dispatchEvent(new Event("locationchange"));
-    return ret;
-  };
+    patterns.forEach(({ pattern }) => {
+      const regex = new RegExp(pattern);
+      if (regex.test(currentPath)) {
+        events.forEach((event) => {
+          console.log("adding event listeners");
+          document.addEventListener(event, throttleAddTime);
+        });
 
-  window.addEventListener("popstate", () => {
-    window.dispatchEvent(new Event("locationchange"));
-  });
+        document.addEventListener("visibilitychange", handleVisibilityChange);
+
+        startTime = Date.now();
+        endTime = updateEndTime(startTime, INTERVAL_TIME);
+        startInterval();
+        return;
+      }
+    });
+  }
+
+  // dispatch custom event for the first time on page load to run the event listeners
+  window.dispatchEvent(new Event("locationchange"));
 }
 
 export {
