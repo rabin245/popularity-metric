adb20debcb8a5a6738cc19d294dad94c2fd2268e
rabin245 <drarabin@gmail.com>
2023-07-04 16:39:11 +0545

    add stopTracking function

--- index.js
+++ index.js
@@ -35,7 +35,6 @@ const trackEvent = (eventType, provider, count, delay = 5000) => {
 
   debounceByEventType[eventType](eventType, provider, count);
 };
-
 function getStoreByPopularity() {
   const providerEventsData = store.getValueFromStore(eventWeights);
   const weightedArr = weightedAverage.calculateWeightedAverage(
@@ -48,148 +47,115 @@ function getStoreByPopularity() {
 
 let startTime = null;
 let endTime = null;
-let functionCallFlag = null;
+let intervalId = null;
 
-function trackTimeOnPages({ weight, id = null }) {
+const throttleAddTime = throttle(addTime, CHECK_TIME);
+let currentProviderId = null;
+
+function trackTimeOnPage({ weight, id = null }) {
+  currentProviderId = id;
   startTime = Date.now();
-  functionCallFlag = true;
   endTime = startTime + INTERVAL_TIME;
   // register the time on page event and weight
   registerEventsAndWeights([["time_on_page", weight]]);
 
-  let intervalId = null; // Variable to store the interval ID
-  // start the interval to check if the user if active
-  function startInterval() {
-    console.log("starting interval", intervalId);
-    intervalId = setInterval(() => {
+  // add event listeners
+  events.forEach((event) => {
+    console.log("adding event listeners");
+    document.addEventListener(event, throttleAddTime);
+  });
+
+  document.addEventListener("visibilitychange", handleVisibilityChange);
+  // start tracking
+  startInterval(currentProviderId);
+}
+
+function startInterval(id) {
+  console.log("starting interval", intervalId);
+  intervalId = setInterval(() => {
+    startTime = Date.now();
+    console.log(
+      "running setinterval with startTime:",
+      startTime,
+      endTime,
+      endTime - startTime
+    );
+    // const currentPath = window.location.pathname;
+    if (isUserActive(startTime, endTime)) {
       startTime = Date.now();
       console.log(
-        "running setinterval with startTime:",
+        "inside of start time:",
         startTime,
         endTime,
         endTime - startTime
       );
-      // const currentPath = window.location.pathname;
-      if (isUserActive(startTime, endTime)) {
-        startTime = Date.now();
-        console.log(
-          "inside of start time:",
-          startTime,
-          endTime,
-          endTime - startTime
-        );
-        // checkPathNameAndStoreTime(currentPath, patterns);
-        incrementTimeOnPage(id);
-      }
-    }, CHECK_TIME - 50);
-    console.log("started itnerval", intervalId);
-  }
+      incrementTimeOnPage(id);
+    }
+  }, CHECK_TIME - 50);
+  console.log("started itnerval", intervalId);
+}
 
-  function stopInterval() {
-    console.log(
-      "stopping interval",
-      intervalId,
-      startTime,
-      endTime,
-      endTime - startTime
-    );
-    clearInterval(intervalId);
+function stopInterval() {
+  console.log(
+    "stopping interval",
+    intervalId,
+    startTime,
+    endTime,
+    endTime - startTime
+  );
+  clearInterval(intervalId);
+  startTime = Date.now();
+  intervalId = null;
+}
+
+// restart the interval
+function restartInterval() {
+  if (intervalId) stopInterval();
+  startInterval(currentProviderId);
+}
+
+// restart interval when document is visible
+function handleVisibilityChange() {
+  console.log("visibility changed", document.hidden);
+  if (document.hidden) {
+    endTime = Date.now();
+  } else {
+    restartInterval();
     startTime = Date.now();
-    intervalId = null;
-  }
-
-  // restart the interval
-  function restartInterval() {
-    if (intervalId) stopInterval();
-    startInterval();
-  }
-
-  // instead of starting the interval with the function, use the custom event to start the interval
-  // startInterval();
-
-  // restart interval when document is visible
-  function handleVisibilityChange() {
-    console.log("visibility changed", document.hidden);
-    if (document.hidden) {
-      endTime = Date.now();
-    } else {
-      restartInterval();
-      startTime = Date.now();
-      endTime = updateEndTime(startTime, INTERVAL_TIME);
-    }
-    console.log(startTime, endTime, endTime - startTime);
-  }
-  // document.addEventListener("visibilitychange", handleVisibilityChange);
-
-  function addTime() {
-    console.log(
-      "adding time to active time",
-      startTime,
-      endTime,
-      endTime - startTime
-    );
-    if (!isUserActive(startTime, endTime)) {
-      console.log("user is not active");
-      restartInterval();
-    }
     endTime = updateEndTime(startTime, INTERVAL_TIME);
-    console.log(startTime, endTime, endTime - startTime);
   }
+  console.log(startTime, endTime, endTime - startTime);
+}
 
-  // add time to endTime when user is active on the page
-  // i.e. when user is scrolling, moving mouse, typing, etc.
-  const throttleAddTime = throttle(addTime, CHECK_TIME);
-  // events.forEach((event) => {
-  //   document.addEventListener(event, throttleAddTime);
-  // });
-
-  // dispatch custom event when location changes
-  setupHistoryListener();
-
-  // add time to endTime when location changes
-  window.addEventListener("locationchange", handleLocationChange);
-  function handleLocationChange() {
-    const idTest = functionCallFlag ? null : id;
-
-    console.log(
-      "location chagned with flag",
-      functionCallFlag,
-      "with id",
-      idTest
-    );
-
-    if (intervalId) stopInterval();
-
-    events.forEach((event) => {
-      console.log("removing event listeners");
-      document.removeEventListener(event, throttleAddTime);
-    });
-    document.removeEventListener("visibilitychange", handleVisibilityChange);
-
-    // if the function is called i.e. the page should be tracked
-    if (functionCallFlag) {
-      events.forEach((event) => {
-        console.log("adding event listeners");
-        document.addEventListener(event, throttleAddTime);
-      });
-
-      document.addEventListener("visibilitychange", handleVisibilityChange);
-
-      startTime = Date.now();
-      endTime = updateEndTime(startTime, INTERVAL_TIME);
-      startInterval();
-      functionCallFlag = false;
-      // return;
-    }
+function addTime() {
+  console.log(
+    "adding time to active time",
+    startTime,
+    endTime,
+    endTime - startTime
+  );
+  if (!isUserActive(startTime, endTime)) {
+    console.log("user is not active");
+    restartInterval();
   }
+  endTime = updateEndTime(startTime, INTERVAL_TIME);
+  console.log(startTime, endTime, endTime - startTime);
+}
 
-  // dispatch custom event when the page to be tracked is loaded
-  if (id && functionCallFlag) window.dispatchEvent(new Event("locationchange"));
+function stopTrackingTimeOnPage() {
+  if (intervalId) stopInterval();
+
+  events.forEach((event) => {
+    console.log("removing event listeners");
+    document.removeEventListener(event, throttleAddTime);
+  });
+  document.removeEventListener("visibilitychange", handleVisibilityChange);
 }
 
 export {
   trackEvent,
   registerEventsAndWeights,
   getStoreByPopularity,
-  trackTimeOnPages,
+  trackTimeOnPage as trackTimeOnPages,
+  stopTrackingTimeOnPage,
 };
--- utils.js
+++ utils.js
@@ -1,18 +1,6 @@
 import { addToStore } from "./store";
 
-// export function checkPathNameAndStoreTime(path, patterns) {
-//   patterns.forEach(({ pattern, splitPosition = 2 }) => {
-//     const regex = new RegExp(pattern);
-//     if (regex.test(path)) {
-//       incrementTimeOnPath(path, splitPosition);
-//       return;
-//     }
-//   });
-// }
-
 export function incrementTimeOnPage(id) {
-  // assume path is like /provider/:id
-  // const id = path.split("/")[splitPosition];
   addToStore(id, "time_on_page", 5);
 }
 
@@ -22,24 +10,4 @@ export function isUserActive(startTime, endTime) {
 
 export function updateEndTime(startTime, additionalTime) {
   return startTime + additionalTime;
-}
-
-export function setupHistoryListener() {
-  let oldPushState = history.pushState;
-  history.pushState = function pushState() {
-    let ret = oldPushState.apply(this, arguments);
-    window.dispatchEvent(new Event("locationchange"));
-    return ret;
-  };
-
-  let oldReplaceState = history.replaceState;
-  history.replaceState = function replaceState() {
-    let ret = oldReplaceState.apply(this, arguments);
-    window.dispatchEvent(new Event("locationchange"));
-    return ret;
-  };
-
-  window.addEventListener("popstate", () => {
-    window.dispatchEvent(new Event("locationchange"));
-  });
 }
